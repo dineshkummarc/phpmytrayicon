29f145f43ba8aa25ad6c29886b94bae7441ff0c0
rockethyperlocal <mapmydestination@gmail.com>
2019-08-23 18:21:24 +0530

    updated

--- index.php
+++ index.php
@@ -5,11 +5,9 @@
 	<head>
 		<!-- Bootstrap core CSS -->
 		<link href="library/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		<!-- Custom CSS -->
-		<link rel="stylesheet" href="custom.css" type="text/css" />
 		<style type="text/css">
-      #division {
-        background-color:#ecf0f1;
+      #division1 {
+        background-color:black;
 		padding:10px;
 		margin-bottom:15px;
 		width:100%;
@@ -18,8 +16,16 @@
 		font-size:90%;
 		color:#34495e;
       }
+	  #division2
+	  {
+	  position:fixed;
+	  bottom:0px;
+	  right:15px;
+	  text-align:right;
+	  }
 	  p{
-		  
+	  color:white;
+	  font-size: 1.15em;
 	  }
 	  a{
 		  font-size:30px;
@@ -29,12 +35,14 @@
     </style>
 	</head>
 	<body>		
-		<div style="position:fixed;bottom:0px;right:15px;text-align:right">
+		<div id="division2">
 			<a role="button" data-toggle="collapse" href="#systrayinfo" aria-expanded="false" aria-controls="systrayinfo">
 			<span class="glyphicon glyphicon-dashboard"></span></a>
 			<div class="collapse" id="systrayinfo">
-			<div id="division">
+			<div id="division1">
 			<p>How to use the application</p>
+			<iframe src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+			<br/>
 			<iframe src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
 			</div>
 			</div>
